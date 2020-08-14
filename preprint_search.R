8506bd34c59aba7cb1b525f73bdee5dae0a5a4ff
rmvpaeme <45523344+rmvpaeme@users.noreply.github.com>
2020-08-14 12:32:41 +0200

    update preprint search for biorxiv api

--- preprint_search.R
+++ preprint_search.R
@@ -1,3 +1,4 @@
+
 library(jsonlite)
 library(tidyverse)
 library(bib2df)
@@ -8,24 +9,32 @@ theme_bar<-theme_classic()+theme(axis.text.x = element_text(angle=90, vjust=0.5,
 
 theme_boxplot<-theme_classic()+theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1),strip.background = element_blank(),axis.ticks.x = element_blank(),axis.title.x = element_blank(),axis.line.x = element_blank(),legend.position = "none")
 
-json <- fromJSON("20200630_collection_json.php.json")
-# source https://connect.biorxiv.org/relate/content/181
-json <- as_tibble(json[["rels"]])
-ggplot(json, aes(x = as.Date(rel_date), fill = rel_site)) + geom_bar() + theme_point + labs(title = "all covid papers")
+
+baseurl <- "https://api.biorxiv.org/covid19/"
+pages <- list()
+for(i in seq(from = 0, to = 7607, by = 30)){
+  json_data <- fromJSON(paste0(baseurl, i))
+  message("Retrieving page ", i)
+  pages[[i+1]] <- json_data$collection
+}
+preprint_papers <- rbind_pages(pages)
+preprint_papers <- as_tibble(preprint_papers)
+
+ggplot(preprint_papers, aes(x = as.Date(rel_date), fill = rel_site)) + geom_bar() + theme_point + labs(title = "all covid papers")
 
 search_term <- "^PIMS$|^MIS$|^multisystemic inflammat$|^inflammatory disease$|^systemic inflammat$|^cytokine release$|^kawasaki$|^vasculitis$|^toxic shock$|^shock$"
 
 search_term_a <- "(PIMS|MIS|multisystemic inflammat|inflammatory disease|systemic inflammat|cytokine release|kawasaki|vasculitis|toxic shock|shock)"
 search_term_b <- "(child|adolescen|teen|pediatric|infant|newborn)"
-  
-abs <- json %>% filter(str_detect(rel_abs, regex(search_term_a, ignore_case = TRUE)) & str_detect(rel_abs, regex(search_term_b, ignore_case = TRUE)))
-titles <- json %>% filter(str_detect(rel_title, regex(search_term_a, ignore_case = TRUE)) & str_detect(rel_abs, regex(search_term_b, ignore_case = TRUE)))
+
+abs <- preprint_papers %>% filter(str_detect(rel_abs, regex(search_term_a, ignore_case = TRUE)) & str_detect(rel_abs, regex(search_term_b, ignore_case = TRUE)))
+titles <- preprint_papers %>% filter(str_detect(rel_title, regex(search_term_a, ignore_case = TRUE)) & str_detect(rel_abs, regex(search_term_b, ignore_case = TRUE)))
 
 full <- rbind(abs,titles)
 
 full <- full %>% distinct(rel_doi, .keep_all = TRUE)
-full <- full %>% select(-rel_num_authors)
-colnames(full) <- c("TITLE", "DOI", "NOTE", "ABSTRACT", "AUTHOR", "DATE", "JOURNAL")
+full <- full %>% select(-c(rel_num_authors, rel_link, version, license, category, rel_abs, type))
+colnames(full) <- c("DOI", "TITLE", "DATE", "JOURNAL", "AUTHOR")
 
 full <- full %>% unnest(AUTHOR) 
 full <- full[!duplicated(full$TITLE),]
@@ -34,4 +43,4 @@ full$DATE <- as.Date(full$DATE)
 
 ggplot(full, aes(x = as.Date(DATE), fill = JOURNAL)) + geom_bar() + theme_point + labs(title = "PIMS papers")
 
-df2bib(full, file = "20200630_covid_full.bib")
+df2bib(full, file = "20200814_covid_full.bib")
